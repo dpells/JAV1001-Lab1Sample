60a0a2cabb127230e4383ae010248a94d2d3abf7
David <david.pellegrini@cambriancollege.ca>
2022-09-23 19:36:57 -0400

    Adds meaningful comments

--- Convert.kt
+++ Convert.kt
@@ -3,10 +3,13 @@ fun main(){
   var numericGrade = readln().toInt()
   print("Enter your Name: ")
   var firstName = readln() */
+
+  // Collects input
   print("Enter your grade and name separated by a space: ")
   var (stringGrade, firstName) = readln().split(' ')
   var numericGrade = stringGrade.toInt()
 
+  // prints output based on input ranges
   when(numericGrade){
     in 80..100 -> println(firstName + " achieved A.")
     in 70..80 -> println(firstName + " achieved B.")
